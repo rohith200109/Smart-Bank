4419ab13776c2e9b5fa1275bc85490c9ff2d7740
Rohith <125893137+rohith200109@users.noreply.github.com>
2024-07-21 21:17:17 +0530

    Update SbAccountrepo.java
    updated

--- smart-Bank/src/main/java/com/nt/repo/SbAccountrepo.java
+++ smart-Bank/src/main/java/com/nt/repo/SbAccountrepo.java
@@ -11,4 +11,13 @@ public interface SbAccountrepo extends JpaRepository<SbAccount,Integer> {
 	
 	public List<SbAccount> findByAcNumber(Long acNumber);
 
+	@Query("select id from SbAccount where acNumber=?1 and custName=?2 and accountType=?3")
+	Integer checkingDepositeDetails(Long acNumber,String custName,String accountType);
+	
+	@Query("select count(*) from SbAccount")
+	Long countRecords();
+	
+	@Query("select  id  from SbAccount where aadhaarNumber=?1 ")
+Integer accountchecking(Long aadhaarNumber);
+
 }
